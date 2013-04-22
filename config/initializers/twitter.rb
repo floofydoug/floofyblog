721329bae8eaf5d21e38d40eb92572d6bd5f57a9
Doug Phung <Doug@chillportal.com>
2013-04-21 22:07:48 -0400

    hi

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "JynMuM6fwBOO0lJCkBM1QA"
+  config.consumer_secret = "c0T8KIicttwvbcLQFrMH45UdaItEMiFbd0c9ARFgMo"
+  config.oauth_token = "1340340002-urtaiqyA1stLI5YwothkK09kRCb7Apd4ySUm9GI"
+  config.oauth_token_secret = "6k3nZ0w2vsytFOxzHnCd2JypTAuggQP2d2HvXbVDcs"
+end
