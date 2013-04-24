fe2c0017e20944048c89f847b01545d681c04e27
Alex Thomas <alex@chillportal.com>
2013-04-23 22:19:36 -0400

    Almost there

--- db/migrate/20130424004633_add_upvoted_and_downvoted_to_ip_address.rb
+++
@@ -1,6 +0,0 @@
-class AddUpvotedAndDownvotedToIPAddress < ActiveRecord::Migration
-  def change
-  	add_column :ip_addresses, :upvoted, :boolean, default: false
-  	add_column :ip_addresses, :downvoted, :boolean, default: false
-  end
-end
